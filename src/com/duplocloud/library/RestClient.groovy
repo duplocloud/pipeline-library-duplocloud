3b2e15abfa024e3220eba8a36efcfc6625123948
gk-duplo <85149359+gk-duplo@users.noreply.github.com>
2021-10-07 12:14:00 +0530

    a

--- src/com/duplocloud/library/RestClient.groovy
+++ src/com/duplocloud/library/RestClient.groovy
@@ -19,29 +19,19 @@ def post(String url, String token, Object body){
     OutputStreamWriter writer = new OutputStreamWriter(connection.getOutputStream())
     writer.write(body);
     writer.flush();
-
-    echo "1"
-
     stream = new InputStreamReader(connection.getInputStream(),"UTF-8");
-    echo "1.1";
     res = stream.text;
-    echo "2"
-
-    // execute the POST request
-    def rs = new JsonSlurper().parse(res)
-
-    echo "3. rs"
-
-    responseCode 
-
     connection.disconnect()
-   
+    echo "HTTP Status: ${connection.responseCode}"
     echo "Recieved Resonse:  ${rs}"
     return res
 
   } catch (err) {
-     echo "Error while calling API: ${connection.getErrorStream().text}, Response: ${res}"
-     thow new Exception("Error while calling API", err)
+     echo "Error while calling API"
+     error= connection.getErrorStream().text;
+     echo "HTTP Status:  ${connection.responseCode}"
+     echo "Error:  ${errort}"
+     throw new Exception(error);
   }
 }
 
